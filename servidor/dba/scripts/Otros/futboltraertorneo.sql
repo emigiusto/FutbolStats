4808e87d648ce76c8d9a84f14b52b77b96d83ef4
Emiliano Giusto <emigiusto@gmail.com>
2019-12-28 17:19:31 -0300

    asd

--- servidor/dba/scripts/Otros/futboltraertorneo.sql
+++ servidor/dba/scripts/Otros/futboltraertorneo.sql
@@ -8,33 +8,38 @@ table1.PA, table1.PAC, table1.eficienciaAlltime, table1.asistenciaAlltime, table
 
 from player 
 
-left outer join (select player.id, jugadorresultado.resultado, count(jugadorresultado.resultado) as 'ganados' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidotorneo on partidotorneo.partido_id = jugadorresultado.partido_id where jugadorresultado.resultado = 1 and partidotorneo.torneo_id =4 group by player.id, jugadorresultado.resultado) as TW on player.id = TW.id 
+left outer join (select player.id, jugadorresultado.resultado, count(jugadorresultado.resultado) as 'ganados' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidotorneo on partidotorneo.partido_id = jugadorresultado.partido_id where jugadorresultado.resultado = 1 and partidotorneo.torneo_id = " + torneoId + "  group by player.id, jugadorresultado.resultado) as TW on player.id = TW.id 
 
-left outer join (select player.id, jugadorresultado.resultado, count(jugadorresultado.resultado) as 'perdidos' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidotorneo on partidotorneo.partido_id = jugadorresultado.partido_id where jugadorresultado.resultado = 0 and partidotorneo.torneo_id = 4 group by player.id, jugadorresultado.resultado) as TL on player.id = TL.id 
+left outer join (select player.id, jugadorresultado.resultado, count(jugadorresultado.resultado) as 'perdidos' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidotorneo on partidotorneo.partido_id = jugadorresultado.partido_id where jugadorresultado.resultado = 0 and partidotorneo.torneo_id = " + torneoId + "  group by player.id, jugadorresultado.resultado) as TL on player.id = TL.id 
 
-left outer join (select player.id, jugadorresultado.resultado, count(jugadorresultado.resultado) as 'empatados' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidotorneo on partidotorneo.partido_id = jugadorresultado.partido_id where jugadorresultado.resultado = 2 and partidotorneo.torneo_id = 4 group by player.id, jugadorresultado.resultado) as TE on player.id = TE.id 
-
-
-
-left outer join (select player.id, jugadorresultado.resultado, SUM(partidos.puntosganador) as 'puntosfavorganador', SUM(partidos.puntosperdedor) as 'puntoscontraganador' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidotorneo on partidotorneo.partido_id = jugadorresultado.partido_id JOIN partidos on  partidotorneo.partido_id = partidos.id where jugadorresultado.resultado = 1 and partidotorneo.torneo_id = 4 group by player.id, jugadorresultado.resultado) as TPG on player.id = TPG.id 
-left outer join (select player.id, jugadorresultado.resultado, SUM(partidos.puntosperdedor) as 'puntosfavorperdedor', SUM(partidos.puntosganador) as 'puntoscontraperdedor' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidotorneo on partidotorneo.partido_id = jugadorresultado.partido_id JOIN partidos on  partidotorneo.partido_id = partidos.id where jugadorresultado.resultado = 0 and partidotorneo.torneo_id = 4 group by player.id, jugadorresultado.resultado) as TPP on player.id = TPP.id 
-
+left outer join (select player.id, jugadorresultado.resultado, count(jugadorresultado.resultado) as 'empatados' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidotorneo on partidotorneo.partido_id = jugadorresultado.partido_id where jugadorresultado.resultado = 2 and partidotorneo.torneo_id = " + torneoId + "  group by player.id, jugadorresultado.resultado) as TE on player.id = TE.id 
 
 
 join jugadortorneo on jugadortorneo.jugador_id = player.id 
-
 join torneos on jugadortorneo.torneo_id = torneos.id 
+join (select count(*) as 'totalpartidos' from partidotorneo where torneo_id = " + torneoId + ") as partidostotalestorneo 
 
-join (select count(*) as 'totalpartidos' from partidotorneo where torneo_id = 4) as partidostotalestorneo 
-
-join (select jugadorresultado.jugador_id, count(jugadorresultado.resultado) as 'totalpartidos',SUM(CASE WHEN jugadorresultado.resultado=1 THEN 1 ELSE 0 END) as 'totalganados',SUM(CASE WHEN jugadorresultado.resultado=0 THEN 1 ELSE 0 END) as 'totalperdidos',SUM(CASE WHEN jugadorresultado.resultado=1 THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END) as 'puntosfavor',SUM(CASE WHEN jugadorresultado.resultado=0 THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END) as 'puntoscontra',(SUM(CASE WHEN jugadorresultado.resultado=1 THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END) - SUM(CASE WHEN jugadorresultado.resultado=0 THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END)) as 'diferencia',((SUM(CASE WHEN jugadorresultado.resultado=1 THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END))-(SUM(CASE WHEN jugadorresultado.resultado=0 THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END)))*2 +((SUM(CASE WHEN jugadorresultado.resultado=1 THEN 1 ELSE 0 END)-SUM(CASE WHEN jugadorresultado.resultado=0 THEN 1 ELSE 0 END))*10) as 'momentum' from player left outer join jugadorresultado on player.id = jugadorresultado.jugador_id join (select * from partidos order by id desc limit 6) as tPartidos on tPartidos.id = jugadorresultado.partido_id group by player.id) 
+join (select jugadorresultado.jugador_id, count(jugadorresultado.resultado) as 'totalpartidos',
+	SUM(CASE WHEN jugadorresultado.resultado=1 THEN 1 ELSE 0 END) as 'totalganados',
+    SUM(CASE WHEN jugadorresultado.resultado=0 THEN 1 ELSE 0 END) as 'totalperdidos',
+    SUM(CASE WHEN jugadorresultado.resultado=2 THEN 1 ELSE 0 END) as 'totalempatados',
+    SUM(CASE WHEN (jugadorresultado.resultado = 1 or jugadorresultado.resultado = 2) THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END) as 'puntosfavor',
+    SUM(CASE WHEN (jugadorresultado.resultado = 0 or jugadorresultado.resultado = 2) THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END) as 'puntoscontra',
+    (SUM(CASE WHEN (jugadorresultado.resultado = 1 or jugadorresultado.resultado = 2) THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END) - SUM(CASE WHEN (jugadorresultado.resultado = 0 or jugadorresultado.resultado = 2) THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END)) as 'diferencia',
+    ((SUM(CASE WHEN jugadorresultado.resultado=1 THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END))-(SUM(CASE WHEN jugadorresultado.resultado=0 THEN tPartidos.puntosganador ELSE tPartidos.puntosperdedor END)))*2 +((SUM(CASE WHEN jugadorresultado.resultado=1 THEN 1 ELSE 0 END)-SUM(CASE WHEN jugadorresultado.resultado=0 THEN 1 ELSE 0 END))*10) as 'momentum' 
+    from player 
+    left outer join jugadorresultado on player.id = jugadorresultado.jugador_id 
+    join (select * from partidos order by id desc limit 6) as tPartidos on tPartidos.id = jugadorresultado.partido_id group by player.id) 
 	as tablemomentum on tablemomentum.jugador_id = player.id 
 
 join (select player.id as 'idplayer',
 		((SUM(case when (jugadorresultado.resultado = 0 or jugadorresultado.resultado = 2) then partidos.puntosperdedor else partidos.puntosganador end))/(COUNT(jugadorresultado.resultado))) as 'PA',
 		((SUM(case when (jugadorresultado.resultado = 1 or jugadorresultado.resultado = 2) then partidos.puntosperdedor else partidos.puntosganador end))/(COUNT(jugadorresultado.resultado))) as 'PAC',
-		TW.ganadosalltime, TL.perdidosalltime, TW.ganadosalltime/(TW.ganadosalltime+TL.perdidosalltime) as 'eficienciaAlltime',
-		((TW.ganadosalltime+TL.perdidosalltime)/(partidostotalestodos.total)) as 'asistenciaAllTime' 
+		IFNULL(TW.ganadosalltime,0) as 'ganadosalltime',
+        IFNULL(TL.perdidosalltime,0) as 'perdidosalltime',
+        IFNULL(TE.empatadosAlltime,0) as 'empatadosalltime',
+        IFNULL(TW.ganadosalltime,0)/(IFNULL(TW.ganadosalltime,0)+IFNULL(TL.perdidosalltime,0)+IFNULL(TE.empatadosAlltime,0)) as 'eficienciaAlltime',
+		((IFNULL(TW.ganadosalltime,0)+IFNULL(TL.perdidosalltime,0)+IFNULL(TE.empatadosAlltime,0))/(partidostotalestodos.total)) as 'asistenciaAllTime' 
 	from player 
     join jugadorresultado on player.id = jugadorresultado.jugador_id 
     join partidos on jugadorresultado.partido_id = partidos.id 
@@ -50,9 +55,19 @@ join (select player.id as 'idplayer',
 		join jugadorresultado on player.id = jugadorresultado.jugador_id 
         where jugadorresultado.resultado = 2 group by player.id, jugadorresultado.resultado) as TE on player.id = TE.id
         
-    join (select player.id, ((SUM(case when jugadorresultado.resultado = 0 then partidos.puntosperdedor else partidos.puntosganador end))/(COUNT(jugadorresultado.resultado))) as 		'puntosfavorporpartido' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidos on jugadorresultado.partido_id = partidos.id group by 		player.id) as TPF on player.id = TPF.id  
-    join (select player.id, ((SUM(case when jugadorresultado.resultado = 1 then partidos.puntosperdedor else partidos.puntosganador end))/(COUNT(jugadorresultado.resultado))) as 		'puntoscontraporpartido' from player join jugadorresultado on player.id = jugadorresultado.jugador_id join partidos on jugadorresultado.partido_id = partidos.id group by 		player.id) as TPC on player.id = TPC.id 
-    group by player.id)
+    join (select player.id, 
+		((SUM(case when (jugadorresultado.resultado = 0 or jugadorresultado.resultado = 2) then partidos.puntosperdedor else partidos.puntosganador end))/(COUNT(jugadorresultado.resultado))) as 'puntosfavorporpartido' 
+        from player 
+        join jugadorresultado on player.id = jugadorresultado.jugador_id 
+        join partidos on jugadorresultado.partido_id = partidos.id 
+        group by player.id) as TPF on player.id = TPF.id  
+        
+    join (select player.id, ((SUM(case when (jugadorresultado.resultado = 1 or jugadorresultado.resultado = 2) then partidos.puntosperdedor else partidos.puntosganador end))/(COUNT(			jugadorresultado.resultado))) as 'puntoscontraporpartido' 
+			from player 
+            join jugadorresultado on player.id = jugadorresultado.jugador_id 
+            join partidos on jugadorresultado.partido_id = partidos.id group by player.id) 
+            as TPC on player.id = TPC.id 
+			group by player.id)
 as table1 on player.id = table1.idplayer 
 
-where torneos.id= 4 group by player.id order by eficiencia desc;
+where torneos.id= " + torneoId + " group by player.id order by eficiencia desc;

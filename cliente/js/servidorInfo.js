6b630a69d4f75218e54adf45266a93be2130f0e7
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-02-19 16:30:09 -0300

    ASD

--- cliente/js/servidorInfo.js
+++ cliente/js/servidorInfo.js
@@ -1,6 +1,6 @@
 //ip y puerto al que se le realizaran los pedidos
-//var servidor = 'https://geofutbolstats.herokuapp.com';
-var servidor = 'http://localhost:8081';
+var servidor = 'https://geofutbolstats.herokuapp.com';
+//var servidor = 'http://localhost:8081';
 var torneoactual = 3
 var tipotorneo = 'Geofobal'
 
